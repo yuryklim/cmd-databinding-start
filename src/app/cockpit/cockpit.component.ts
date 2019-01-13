e72d7b079af87399cd1e060f02990d5a2715ea6a
yuryklim <yuryklim@outlook.com>
2019-01-13 23:03:52 +0200

    seeing lifecycle ngOnInit hook in action

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -12,9 +12,12 @@ export class CockpitComponent implements OnInit {
   /*newServerContent = '';*/
   @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor cockpit called!');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit cockpit called!');
   }
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -9,9 +9,12 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log('constructor server-element called!');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit server-element called!');
   }
 
 }
